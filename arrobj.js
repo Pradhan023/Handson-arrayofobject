63e0df0bdccef35428fd107bfc607a93931cf131
Anish Pradhan <anishpradha5523@gmail.com>
2023-05-30 18:02:48 +0530

    Handson Array-of-object

---
+++ arrobj.js
@@ -0,0 +1,95 @@
+let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+          {name: 'Baba', id: 101, marks : 23 },
+          {name: 'yaga', id: 200, marks : 45 },
+          {name: 'Wick', id: 115, marks : 75 } ] 
+
+    // Q1
+    // let nn = studentRecords.map(nb=>{
+    //     return(nb.name)
+    // })
+    // console.log(nn);
+
+    // Q2
+
+    // let nn = studentRecords.filter(nb=>{
+    //     if(nb.marks>50)
+    //     {
+    //         return nb
+    //     }
+    // })
+    // console.log(nn);
+
+    // Q3
+
+    // let nn = studentRecords.filter(nb=>{
+    //     if(nb.marks>50 && nb.id>120)
+    //     {
+    //         return nb
+    //     }
+    // })
+    // console.log(nn);
+
+    // Q4
+    // let nn = studentRecords.map(nb =>{
+    //     return nb.marks
+    // })
+    // const sum = nn.reduce((cu,ab) => {
+    //     return cu+ab
+    // })
+
+    // console.log(`Total marks of students is : ${sum}`);
+
+    // Q5
+
+    // let nn = studentRecords.filter(nb=>{
+    //     if(nb.marks>50)
+    //     {
+    //         console.log(nb.name);
+    //     }
+    // })
+
+    // Q6
+
+    // let nn = studentRecords.filter(nb =>{
+    //         if(nb.id>120)
+    //         {
+    //             return (nb.marks)
+    //         }
+    //     })
+    //     const sum = nn.reduce((cu,ab) => {
+    //         console.log (`Sum of the marks of Student with ID greater than 120 is : ${cu.marks+ab.marks}`)
+    //     })
+
+    // Q7
+    
+    // let nm = studentRecords.map( n =>{
+    //     if(n.marks<50)
+    //     {
+    //         return(n.marks+15)
+    //     }
+    //     else{
+    //         return(n.marks)
+    //     }
+    // })
+    
+    // const fs = nm.filter(marks =>{
+    //     if(marks>50)
+    //     {
+    //         return marks
+    //     }
+    // })
+    // const total = fs.reduce((cur,acc) =>{
+    //     return cur+acc
+    // })
+    // console.log(`Total marks of the students with marks greater than 50 after a grace of 15 marks has been added to those students who scored less than 50 is - ${total}`);
+
+    // Q8 
+
+    // const arr = [
+    //     { name : 'Anish' , class : 'Btech CSE' , rollno : 08 },
+    //     { name : 'Sumit' , class : 'Btech CSE' , rollno : 35 },
+    //     { name : 'Prince' , class : 'Btech CSE' , rollno : 21 },
+    //     { name : 'Nikhil' , class : 'Btech CSE' , rollno : 25},
+    //     { name : 'Himanshi' , class : 'Btech CSE' , rollno : 13 },
+    //     { name : 'Kamini' , class : 'Btech CSE' , rollno : 11 }
+    // ]
